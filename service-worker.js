29ce8c51cd03cd7451aef35fb8c5b21d7fce6c66
naufalamru <naufalamru071003@gmail.com>
2026-01-13 19:07:51 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("timer-cache").then(cache =>
+      cache.addAll([
+        "timer.html",
+        "manifest.json",
+        "sound/BELL.MP3",
+        "sound/KO.MP3"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
