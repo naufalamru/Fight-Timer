fbd0fa1d653d44b760f1765589db290c12f07ef8
naufalamru <naufalamru071003@gmail.com>
2026-01-13 19:46:41 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,25 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("timer-cache").then(cache =>
-      cache.addAll([
-        "index.html",
-        "manifest.json",
-        "sound/BELL.MP3",
-        "sound/KO.MP3"
-      ])
-    )
+const CACHE_NAME = "fight-timer-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png",
+        "./sound/bell.mp3",
+        "./sound/ko.mp3"
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
-
